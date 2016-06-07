350f1932cd421bce85f3e43455249255408ef388
Handsome <535091118@qq.com>
2016-06-07 18:36:25 +0800

    normal submit

--- swap.erl
+++ swap.erl
@@ -1,30 +1,35 @@
 -module(swap).
+-import(market_c, [current_price/0]).
 -export([daemon/0]).
 
 daemon()->
-    check_status(),
-    get_prices(),
-    make_decision(),
-    do_deal(),
+    Decision = make_decision(),
+    do_deal(Decision),
     nil.
     %timer:sleep(1000),
     %daemon().
 
-check_status()->
-    log("check status"),
-    nil.
-
-get_prices()->
-    log("get prices"),
-    nil.
-
 make_decision()->
+    {C_ccount_status, H_ccount_status} = get_account_status(),}
+    get_prices(),
     log("make decision"),
     nil.
 
-do_deal()->
+do_deal(Decision)->
     log("do deal"),
     nil.
 
-log(Msg)->
+lget_account_status()->
+    log("check status"),
+    C_ccount_status = market_c::get_account_status(),
+    H_ccount_status = market_h::get_account_status(),
+    {C_ccount_status, H_ccount_status}.
+
+get_prices()->
+    log("get prices"),
+    Price_c = market_c:current_price(),
+    Price_h = market_h:current_price(),
+    {Price_c, Price_h}.
+
+og(Msg)->
     io:format('~p~n', [Msg]).
