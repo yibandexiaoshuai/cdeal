0744a198fbde68f3714c19997846fc6213683a35
Handsome <535091118@qq.com>
2016-06-07 18:35:41 +0800

    normal submit

---
+++ market_c.erl
@@ -0,0 +1,17 @@
+-module(market_c).
+-export([current_price/0]).
+
+current_price()->
+    nil.
+
+buy(Amount, Price)->
+    nil.
+
+sell(Amount, Price)->
+    nil.
+
+order(Type, Amount, Price)->
+    nil.
+
+get_account_status()->
+    nil.
